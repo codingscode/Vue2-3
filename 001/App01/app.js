bb8fbe050421309fd21cb8ccb1789a6bc4400376
codingscode <heronnestg@hotmail.com>
2021-09-02 23:09:37 -0300

    javascript

---
+++ 001/App01/app.js
@@ -0,0 +1,14 @@
+
+const elemento_botao = document.querySelector('button');
+const elemento_entrada = document.querySelector('input');
+const elemento_lista = document.querySelector('ul');
+
+elemento_botao.addEventListener('click', () => {
+   const valor = elemento_entrada.value
+   const elemento_itemlista = document.createElement('li')
+   elemento_itemlista.textContent = valor
+   elemento_lista.appendChild(elemento_itemlista)
+})
+
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Vue 2 & 3
+## Acompanhamento de curso
+
+
+
+
