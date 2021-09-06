61489056638b8ef9394c95526277984259402e3f
codingscode <heronnestg@hotmail.com>
2021-09-06 20:40:53 -0300

    parte 17*

--- 001/vue/src/App.vue
+++ 001/vue/src/App.vue
@@ -1,7 +1,10 @@
 <template>
    <div id="app" >
-      <h1>{{ titulo }}</h1>
-      {{ mostrarTitulo() }}
+      <h1 v-once >{{ titulo }}</h1>
+      <p>
+         {{ mostrarTitulo() }}
+      </p>
+      
    </div>
 
 </template>
