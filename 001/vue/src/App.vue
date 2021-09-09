78ed40b3ca95e19e7aa656843b96718cf8b5da03
codingscode <heronnestg@hotmail.com>
2021-09-08 21:26:47 -0300

    parte 30

--- 001/vue/src/App.vue
+++ 001/vue/src/App.vue
@@ -1,53 +1,30 @@
 <template>
    <div id="app" >
-      <h1 v-once >
-         {{ titulo }}
-      </h1>
-      <p>
-         {{mostrarTitulo()}}
-      </p>
-      <p v-html="vinculoHTML" ></p>
-      <input type="text" v-model="contadorInicial" @keyup.enter="setarContador" >
-      <button @click="aumentarContador(2)" >Somar +2</button>
-      <p>{{ contador }}</p>
-      <p @mousemove="atualizarValores" class="aqui" >
-         Valor de X: {{ x }}<br>
-         Valor de Y: {{ y }}
-      </p>
+     {{ 'olá vue' | capitalize }}
+      
    </div>
 
 </template>
 
 <script>
-
+/*
+Vue.filter('capitalize', function(string) {
+   var primeiroCaract = string.charAt(0).toUpperCase()
+   var restoCaract = string.slice(1, string.length)
+   return primeiroCaract + restoCaract
+})
+*/
 
 export default {
    name: 'App',
    data() {
       return {
-         titulo: 'olá mundo com vue',
-         vinculoHTML: '<a href="https://codigoonclick.com" >Código OnClick</a>',
-         contador: 1,
-         contadorInicial: 0,
-         x: 0,
-         y: 0
+         
       }
    },
    methods: {
-      mostrarTitulo() {
-         this.titulo = 'olá deste methods'
-         return this.titulo
-      },
-      aumentarContador(valor) {
-         this.contador += valor
-      },
-      setarContador() {
-         this.contador = this.contadorInicial
-      },
-      atualizarValores() {
-         this.x = event.clientX
-         this.y = event.clientY
-      }
+      
+      
    },
    computed: {
       
@@ -62,12 +39,6 @@ export default {
 </script>
 
 <style>
-#app {
-   border: 3px solid lightblue;
-}
 
-.aqui {
-   border: 3px solid lightgreen;
-}
 
 </style>
--- 001/vue/src/main.js
+++ 001/vue/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('capitalize', function(string) {
+   var primeiroCaract = string.charAt(0).toUpperCase()
+   var restoCaract = string.slice(1, string.length)
+   return primeiroCaract + restoCaract
+})
+
+
 new Vue({
    render: h => h(App),
 }).$mount('#app')
