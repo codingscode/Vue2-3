79f4655cf7b90104e8f8f1a9edda33be59388324
codingscode <heronnestg@hotmail.com>
2021-09-08 23:01:37 -0300

    parte 39

--- 001/vue/src/App.vue
+++ 001/vue/src/App.vue
@@ -5,11 +5,15 @@
             <tr>
                <th>Nome</th>
                <th>País</th>
-               <th>Eletricidade</th>                              
+               <th>Eletricidade</th>                      
             </tr>
          </thead>
          <tbody>
-         
+            <tr v-for="(cada, i) in represas" :key="i" >
+               <td >{{ cada.nome }}</td>
+               <td >{{ cada.pais }}</td>
+               <td >{{ cada.eletricidade }} MegaWatts </td>
+            </tr>
          </tbody>
       </table>
    </div>
@@ -27,9 +31,9 @@ export default {
    name: 'App',
    data() {
       return {
-         represa: [
+         represas: [
             {nome: 'Represa Nurek', pais: 'Tajikistão', eletricidade: 32000},
-            {nome: 'Tres gargantas', pais: 'China', eletricidade: 22500},
+            {nome: 'Represa Tres gargantas', pais: 'China', eletricidade: 22500},
             {nome: 'Represa Tarbela', pais: 'Paquistão', eletricidade: 3500},
             {nome: 'Represa Guri', pais: 'Venezuela', eletricidade: 10200}                                    
          ] 
