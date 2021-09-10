1237a2c0fc14b40db83a059f169e77c83ee8767f
codingscode <heronnestg@hotmail.com>
2021-09-09 21:09:44 -0300

    parte 41

--- 001/vue/src/App.vue
+++ 001/vue/src/App.vue
@@ -1,22 +1,6 @@
 <template>
    <div id="app" >
-      <table>
-         <thead>
-            <tr>
-               <th>Nome</th>
-               <th>País</th>
-               <th>Eletricidade</th>                      
-            </tr>
-         </thead>
-         <tbody>
-            <tr v-for="(cada, i) in represasPorEletricidade" :key="i" >
-               <td >{{ cada.nome }}</td>
-               <td >{{ cada.pais }}</td>
-               <td >{{ cada.eletricidade }} MegaWatts </td>
-            </tr>
-         </tbody>
-      </table>
-     
+      A carteira está {{estaAtivo === 'ativado' ? 'ativa' : 'desativada' }}     
    </div>
 
 </template>
@@ -32,18 +16,13 @@ export default {
    name: 'App',
    data() {
       return {
-         represas: [
-            {nome: 'Represa Nurek', pais: 'Tajikistão', eletricidade: 32000},
-            {nome: 'Represa Tres gargantas', pais: 'China', eletricidade: 22500},
-            {nome: 'Represa Tarbela', pais: 'Paquistão', eletricidade: 3500},
-            {nome: 'Represa Guri', pais: 'Venezuela', eletricidade: 10200}                                    
-         ] 
+         estaAtivo: 'ativado'
+         
       }
    },
    computed: {
-      represasPorEletricidade() {
-         return [...this.represas].sort((a, b) => b.eletricidade - a.eletricidade)  // só funciona com a cópia
-      }
+
+      
    },
    methods: {
      
