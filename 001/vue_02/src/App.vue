1f354045b3d77c57e95752eebe240cea23577317
codingscode <heronnestg@hotmail.com>
2021-09-10 22:32:53 -0300

    parte 44

--- 001/vue_02/src/App.vue
+++ 001/vue_02/src/App.vue
@@ -1,22 +1,25 @@
 <template>
    <div>
       Vue 3
-
+      <Outro />
+      <outro />
    </div>
 </template>
 
 <script>
 
+import Outro from './components/Outro.vue'
 
 export default {
   name: 'App',
   components: {
-    
+     Outro  
   }
+
 }
 </script>
 
-<style>
+<style  >
 #app {
   
 }
---
+++ 001/vue_02/src/components/Outro.vue
@@ -0,0 +1,27 @@
+<template>
+   <div>
+      <div class="classe" >
+          Outro componente
+      </div>
+      <div>
+          mais conteudo
+      </div>
+   </div>
+
+</template>
+
+<script>
+
+
+export default {
+   name: 'Outro'
+   
+}
+
+</script>
+
+<style scope>
+   div {
+       border: solid 2px lightblue;
+   }
+</style>
