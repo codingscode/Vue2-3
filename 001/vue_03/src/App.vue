7f2e6ca33666b04f597656f79d3433f6c5a06082
codingscode <heronnestg@hotmail.com>
2021-09-12 21:40:56 -0300

    parte 67

---
+++ 001/vue_03/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+   <div>
+      Vue
+   </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+  components: {
+     
+  },
+  data() {
+     return {
+        
+     }
+  },
+  computed: {
+     
+  },
+  methods: {
+    
+  }
+
+}
+</script>
+
+<style  >
+
+   body {
+      font-family: arial;
+   
+   }
+
+
+
+</style>
