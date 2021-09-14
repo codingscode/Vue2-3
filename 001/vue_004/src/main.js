5caab1e6cf74eac6995355660c731cc93e39abdb
codingscode <heronnestg@hotmail.com>
2021-09-14 20:04:11 -0300

    parte 78

--- 001/vue_004/src/main.js
+++ 001/vue_004/src/main.js
@@ -3,17 +3,23 @@ import App from './App'
 
 Vue.config.productionTip = false
 
-Vue.component('saludo', {
+var componente1 = Vue.extend({
    template: '<h1>Olá - Introdução a componentes</h1>'
 })
 
-Vue.component('titulo', {
+var componente2 = Vue.extend({
    template: '<h3>Lista de Frameworks</h3>'
 })
 
-Vue.component('lista-frameworks', {
+var componente3 = Vue.extend({
    template: '<ul><li>Vue.js</li><li>Angular</li><li>ReactJs</li></ul>'
 })
+
+
+Vue.component('componente-saludo', componente1)
+Vue.component('componente-titulo', componente2)
+Vue.component('componente-lista', componente3)
+
 
 new Vue({
   el: '#app'
