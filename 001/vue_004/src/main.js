694f3914eedfbe558fbe0c22e8c19965b041a0f9
codingscode <heronnestg@hotmail.com>
2021-09-13 23:25:58 -0300

    parte 77

--- 001/vue_004/src/main.js
+++ 001/vue_004/src/main.js
@@ -7,6 +7,14 @@ Vue.component('saludo', {
    template: '<h1>Olá - Introdução a componentes</h1>'
 })
 
+Vue.component('titulo', {
+   template: '<h3>Lista de Frameworks</h3>'
+})
+
+Vue.component('lista-frameworks', {
+   template: '<ul><li>Vue.js</li><li>Angular</li><li>ReactJs</li></ul>'
+})
+
 new Vue({
   el: '#app'
   
