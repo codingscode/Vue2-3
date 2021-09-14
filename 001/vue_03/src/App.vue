5808a09398f604ad7393cee1de3640a829c1473a
codingscode <heronnestg@hotmail.com>
2021-09-13 21:39:16 -0300

    parte 71

--- 001/vue_03/src/App.vue
+++ 001/vue_03/src/App.vue
@@ -10,9 +10,22 @@
            <label  >Texto</label>
            <textarea class="form-control" v-model="nota.texto" ></textarea>
         </div>
+       <button class="btn btn-primary mt-2" @click="adicionarNota" >Enviar</button>
 
      </div>
-     <button class="btn btn-primary" @click="adicionarNota" >Enviar</button>
+     <div class="col-sm-12" >
+        <div class="col-sm-4 nota" v-for="(cada, i) in notas" :key="i" >
+           <div class="card" >
+             <div class="card-block" >
+                <div class="card-title" >
+                   {{ cada.titulo }}
+                </div>
+                <div class="card-subtitle mb-2 text-muted" >{{ cada.encontro }}</div>
+                <div class="card-text" >{{ cada.texto }}</div>    
+             </div>   
+           </div>
+        </div>
+     </div>
   </div>  
 </template>
 
@@ -24,14 +37,14 @@ export default {
        titulo: 'Gestão de notas',
        nota: { titulo: '', texto: '' },
        notas: [
-          { titulo: 'ir ao cinema', texto: 'examinar filmes de estreia', encontro: new Date(Date.now()).toLocaleDateString() }
+          { titulo: 'ir ao cinema', texto: 'examinar filmes de estreia', encontro: new Date(Date.now()).toLocaleString() }
        ]
     }
   },
   methods:{
      adicionarNota() {
         let { texto, titulo } = this.nota
-        this.notas.push({ texto, titulo, fecha: new Date(Date.now()).toLocaleString() })
+        this.notas.push({ texto, titulo, encontro: new Date(Date.now()).toLocaleString() })
      }
   }
 }
@@ -42,5 +55,19 @@ export default {
    .form {
       text-align: left;
    }
+  
+   .card {
+      text-align: left;
+      border: 1px solid #2c3e50;
+      border-radius: 4px;
+      padding-left: 8px;
+      padding-right: 8px; 
+   }
+   
+   .nota {
+      padding: 5px;
+   }
+   
+   
   
 </style>
