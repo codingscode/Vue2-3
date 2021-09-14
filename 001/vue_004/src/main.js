8ed4e4b70bbd441035dfd87fb0ff181459e156c1
codingscode <heronnestg@hotmail.com>
2021-09-13 23:18:10 -0300

    parte 76

--- 001/vue_004/src/main.js
+++ 001/vue_004/src/main.js
@@ -1,13 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.component('saludo', {
+   template: '<h1>Olá - Introdução a componentes</h1>'
+})
+
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+  el: '#app'
+  
 })
